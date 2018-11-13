878635cf5d4258c8ab7ab4cab98932dd6c1f20e3
git <1391930327@qq.com>
2018-11-13 18:43:38 +0800

    working version commit

---
+++ data/js/0.js
@@ -0,0 +1,11 @@
+<!--
+document.write("·<a href='/a/webbase/dreamweaver/2010/0408/106.html'>{dede:招聘启事 标题</a><br>");
+document.write("·<a href='/plus/view.php?aid=98'>DedeCMS产品授权在线购</a><br>");
+document.write("·<a href='/plus/view.php?aid=97'>DedeCMS 最终用户授权协</a><br>");
+document.write("·<a href='/a/pics/2010/0407/96.html'>我的眼界-一些关于花</a><br>");
+document.write("·<a href='/a/pics/2010/0407/95.html'>原创风景图片</a><br>");
+document.write("·<a href='/a/pics/2010/0407/94.html'>武夷山的美景真是不</a><br>");
+document.write("·<a href='/a/pics/2010/0407/93.html'>海岛风光</a><br>");
+document.write("·<a href='/a/webbase/html/2010/0407/92.html'>DedeCMS V5.6 开发日志及</a><br>");
+
+-->
